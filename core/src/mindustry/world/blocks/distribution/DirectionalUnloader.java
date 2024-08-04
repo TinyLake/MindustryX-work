TinyLake/MindustryX

C(RenderExt) arcChoiceUiIcon(显示选中物品图标)

core/src/mindustry/world/blocks/distribution/DirectionalUnloader.java
@@ -14,6 +14,7 @@ import mindustry.world.blocks.*;
 import mindustry.world.blocks.storage.CoreBlock.*;
 import mindustry.world.blocks.storage.StorageBlock.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -122,6 +123,7 @@ public class DirectionalUnloader extends Block{
                 Draw.color(unloadItem.color);
                 Draw.rect(centerRegion, x, y);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(unloadItem.uiIcon, x, y,4f,4f);
             }else{
                 Draw.rect(arrowRegion, x, y, rotdeg());
             }

core/src/mindustry/world/blocks/distribution/DuctRouter.java
@@ -12,6 +12,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -84,6 +85,7 @@ public class DuctRouter extends Block{
                 Draw.color(sortItem.color);
                 Draw.rect("center", x, y);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(sortItem.uiIcon, x, y,4f,4f);
             }else{
                 Draw.rect(topRegion, x, y, rotdeg());
             }

core/src/mindustry/world/blocks/distribution/Sorter.java
@@ -13,6 +13,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -78,6 +79,7 @@ public class Sorter extends Block{
                 Draw.color(sortItem.color);
                 Fill.square(x, y, tilesize/2f - 0.00001f);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(sortItem.uiIcon, x, y,4f,4f);
             }
 
             super.draw();

core/src/mindustry/world/blocks/sandbox/ItemSource.java
@@ -11,6 +11,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -73,6 +74,7 @@ public class ItemSource extends Block{
                 Draw.color(outputItem.color);
                 Fill.square(x, y, tilesize/2f - 0.00001f);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(outputItem.uiIcon, x, y,4f,4f);
             }
 
             super.draw();

core/src/mindustry/world/blocks/storage/Unloader.java
@@ -17,6 +17,8 @@ import mindustry.world.meta.*;
 
 import java.util.*;
 
+import mindustryX.features.*;
+
 import static mindustry.Vars.*;
 
 public class Unloader extends Block{
@@ -217,6 +219,10 @@ public class Unloader extends Block{
         public void draw(){
             super.draw();
 
+            if(RenderExt.arcChoiceUiIcon && sortItem != null){
+                Draw.rect(sortItem.uiIcon, x, y,4f,4f);
+                return;
+            }
             Draw.color(sortItem == null ? Color.clear : sortItem.color);
             Draw.rect(centerRegion, x, y);
             Draw.color();

core/src/mindustry/world/blocks/units/UnitCargoUnloadPoint.java
@@ -12,6 +12,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -47,6 +48,7 @@ public class UnitCargoUnloadPoint extends Block{
                 Draw.color(item.color);
                 Draw.rect(topRegion, x, y);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(item.uiIcon, x, y,4f,4f);
             }
         }
 

TinyLake/MindustryX-work

core/src/mindustry/world/blocks/distribution/DirectionalUnloader.java
@@ -14,6 +14,7 @@ import mindustry.world.blocks.*;
 import mindustry.world.blocks.storage.CoreBlock.*;
 import mindustry.world.blocks.storage.StorageBlock.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -122,6 +123,7 @@ public class DirectionalUnloader extends Block{
                 Draw.color(unloadItem.color);
                 Draw.rect(centerRegion, x, y);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(unloadItem.fullIcon, x, y,4f,4f);
             }else{
                 Draw.rect(arrowRegion, x, y, rotdeg());
             }

core/src/mindustry/world/blocks/distribution/DuctRouter.java
@@ -12,6 +12,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -84,6 +85,7 @@ public class DuctRouter extends Block{
                 Draw.color(sortItem.color);
                 Draw.rect("center", x, y);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(sortItem.fullIcon, x, y,4f,4f);
             }else{
                 Draw.rect(topRegion, x, y, rotdeg());
             }

core/src/mindustry/world/blocks/distribution/Sorter.java
@@ -13,6 +13,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -78,6 +79,7 @@ public class Sorter extends Block{
                 Draw.color(sortItem.color);
                 Fill.square(x, y, tilesize/2f - 0.00001f);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(sortItem.fullIcon, x, y,4f,4f);
             }
 
             super.draw();

core/src/mindustry/world/blocks/sandbox/ItemSource.java
@@ -11,6 +11,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -73,6 +74,7 @@ public class ItemSource extends Block{
                 Draw.color(outputItem.color);
                 Fill.square(x, y, tilesize/2f - 0.00001f);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(outputItem.fullIcon, x, y,4f,4f);
             }
 
             super.draw();

core/src/mindustry/world/blocks/storage/Unloader.java
@@ -18,6 +18,8 @@ import mindustry.world.meta.*;
 
 import java.util.*;
 
+import mindustryX.features.*;
+
 import static mindustry.Vars.*;
 
 public class Unloader extends Block{
@@ -228,6 +230,10 @@ public class Unloader extends Block{
         public void draw(){
             super.draw();
 
+            if(RenderExt.arcChoiceUiIcon && sortItem != null){
+                Draw.rect(sortItem.fullIcon, x, y,4f,4f);
+                return;
+            }
             Draw.color(sortItem == null ? Color.clear : sortItem.color);
             Draw.rect(centerRegion, x, y);
             Draw.color();

core/src/mindustry/world/blocks/units/UnitCargoUnloadPoint.java
@@ -12,6 +12,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.*;
 import mindustry.world.meta.*;
+import mindustryX.features.*;
 
 import static mindustry.Vars.*;
 
@@ -47,6 +48,7 @@ public class UnitCargoUnloadPoint extends Block{
                 Draw.color(item.color);
                 Draw.rect(topRegion, x, y);
                 Draw.color();
+                if(RenderExt.arcChoiceUiIcon) Draw.rect(item.fullIcon, x, y,4f,4f);
             }
         }
 