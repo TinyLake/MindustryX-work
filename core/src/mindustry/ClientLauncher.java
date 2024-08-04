TinyLake/MindustryXBuilds

C: Portable Data Dir

core/src/mindustry/ClientLauncher.java
@@ -36,6 +36,9 @@ public abstract class ClientLauncher extends ApplicationCore implements Platform
     @Override
     public void setup(){
         String dataDir = OS.env("MINDUSTRY_DATA_DIR");
+        if(app.isDesktop() && dataDir == null && Fi.get("config").isDirectory()){
+            dataDir = "config";
+        }
         if(dataDir != null){
             Core.settings.setDataDirectory(files.absolute(dataDir));
         }

TinyLake/MindustryX-work

core/src/mindustry/ClientLauncher.java
@@ -36,6 +36,9 @@ public abstract class ClientLauncher extends ApplicationCore implements Platform
     @Override
     public void setup(){
         String dataDir = OS.env("MINDUSTRY_DATA_DIR");
+        if(app.isDesktop() && dataDir == null && Fi.get("data").isDirectory()){
+            dataDir = "data";
+        }
         if(dataDir != null){
             Core.settings.setDataDirectory(files.absolute(dataDir));
         }