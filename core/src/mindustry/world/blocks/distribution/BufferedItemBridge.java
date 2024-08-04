TinyLake/MindustryX

C(RenderExt) hiddenItemTransparency(桥梁交叉器透视)

core/src/mindustry/world/blocks/distribution/BufferedItemBridge.java
@@ -1,9 +1,16 @@
 package mindustry.world.blocks.distribution;
 
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.util.*;
 import arc.util.io.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustryX.features.*;
+
+import static mindustry.Vars.*;
 
 public class BufferedItemBridge extends ItemBridge{
     public final int timerAccept = timers++;
@@ -39,6 +46,44 @@ public class BufferedItemBridge extends ItemBridge{
         public void doDump(){
             dump();
         }
+
+        @Override
+        public void draw(){
+            super.draw();
+
+            if(RenderExt.hiddenItemTransparency > 0){
+                Tile other = world.tile(link);
+
+                float begx, begy, endx, endy;
+                if(!linkValid(tile, other)){
+                    begx = x - tilesize / 2f;
+                    begy = y - tilesize / 2f;
+                    endx = x + tilesize / 2f;
+                    endy = y - tilesize / 2f;
+                }else{
+                    int i = tile.absoluteRelativeTo(other.x, other.y);
+                    float ex = other.worldx() - x - Geometry.d4(i).x * tilesize / 2f,
+                    ey = other.worldy() - y - Geometry.d4(i).y * tilesize / 2f;
+                    float warmup = state.isEditor() ? 1f : this.warmup;
+                    ex *= warmup;
+                    ey *= warmup;
+
+                    begx = x + Geometry.d4(i).x * tilesize / 2f;
+                    begy = y + Geometry.d4(i).y * tilesize / 2f;
+                    endx = x + ex;
+                    endy = y + ey;
+                }
+                Item item;
+                for(int i = 0; (item = buffer.getItem(i)) != null; i++){
+                    float time = buffer.getTime(i);
+                    float p = Math.min(((Time.time - time) * timeScale / speed), (float)(bufferCapacity - i) / bufferCapacity);
+
+                    Draw.alpha(RenderExt.hiddenItemTransparency / 100f);
+                    Draw.rect(item.uiIcon, Mathf.lerp(begx, endx, p), Mathf.lerp(begy, endy, p), 4f, 4f);
+                }
+            }
+        }
+
 
         @Override
         public void write(Writes write){

TinyLake/MindustryX-work

core/src/mindustry/world/blocks/distribution/BufferedItemBridge.java
@@ -1,9 +1,17 @@
 package mindustry.world.blocks.distribution;
 
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.util.*;
 import arc.util.io.*;
 import mindustry.gen.*;
+import mindustry.graphics.*;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustryX.features.*;
+
+import static mindustry.Vars.*;
 
 public class BufferedItemBridge extends ItemBridge{
     public final int timerAccept = timers++;
@@ -39,6 +47,45 @@ public class BufferedItemBridge extends ItemBridge{
         public void doDump(){
             dump();
         }
+
+        @Override
+        public void draw(){
+            super.draw();
+
+            if(RenderExt.hiddenItemTransparency > 0){
+                Draw.z(Layer.power + 0.1f);
+                Tile other = world.tile(link);
+
+                float begx, begy, endx, endy;
+                if(!linkValid(tile, other)){
+                    begx = x - tilesize / 2f;
+                    begy = y - tilesize / 2f;
+                    endx = x + tilesize / 2f;
+                    endy = y - tilesize / 2f;
+                }else{
+                    int i = tile.absoluteRelativeTo(other.x, other.y);
+                    float ex = other.worldx() - x - Geometry.d4(i).x * tilesize / 2f,
+                    ey = other.worldy() - y - Geometry.d4(i).y * tilesize / 2f;
+                    float warmup = state.isEditor() ? 1f : this.warmup;
+                    ex *= warmup;
+                    ey *= warmup;
+
+                    begx = x + Geometry.d4(i).x * tilesize / 2f;
+                    begy = y + Geometry.d4(i).y * tilesize / 2f;
+                    endx = x + ex;
+                    endy = y + ey;
+                }
+                Item item;
+                for(int i = 0; (item = buffer.getItem(i)) != null; i++){
+                    float time = buffer.getTime(i);
+                    float p = Math.min(((Time.time - time) * timeScale / speed), (float)(bufferCapacity - i) / bufferCapacity);
+
+                    Draw.alpha(RenderExt.hiddenItemTransparency / 100f);
+                    Draw.rect(item.fullIcon, Mathf.lerp(begx, endx, p), Mathf.lerp(begy, endy, p), 4f, 4f);
+                }
+            }
+        }
+
 
         @Override
         public void write(Writes write){